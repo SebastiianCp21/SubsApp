53d9fd8aec4733a3144c8e81d042d99049e1cc85
SebastiianCp21 <sebastiancruz2534@gmail.com>
2025-06-10 18:46:53 -0500

    Se corrige error de escritura

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Swal from 'sweetalert2'
-import '../styles/register.css'
+import '../styles/Register.css'
 
 function Register() {
   const navigate = useNavigate()
